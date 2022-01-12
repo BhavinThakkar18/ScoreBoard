5b2fe64233ff68da7c475014df2e1f22c1c007f8
BhavinThakkar18 <bhavin.majethia18@gmail.com>
2022-01-12 18:23:21 +0530

    removed multiple return statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,23 @@ package com.company;
 public class Main {
     public static int calculateHighScorePosition(int score)
     {
+        int position =4;
         if(score>=1000)
         {
-            return 1;
-        }
-        else if(score>=500)
+            position=1;
+        } else if(score >=500)
         {
-            return 2;
+            position=2;
         }
         else if(score >= 100)
         {
-            return 3;
+            position=3;
         }
-        else
-        {
-            return 4;
-        }
+        return position;
     }
     public static void displayHighScorePosition(String name,int position)
     {
-        System.out.println(name+"  has  managed to get into position = " + position);
+        System.out.println(name+"  has  managed to get into  " + position +" position on ScoreBoard.");
     }
 
     public static void main(String[] args) {
